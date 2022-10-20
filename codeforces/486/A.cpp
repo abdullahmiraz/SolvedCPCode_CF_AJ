8e293f1ee7925beb0b0a0c128045f408ac85e756
Abdullah Miraz <abdullahmiraz2015@gmail.com>
2022-10-20 18:42:00 +0600

    Add solution for problem `A - Calculating Function` Link: https://codeforces.com/contest/486/problem/A Tags: implementation, math, *800 Ref: https://codeforces.com/contest/486/submission/177128874

--- codeforces/486/A.cpp
+++ codeforces/486/A.cpp
@@ -4,11 +4,16 @@ using ll = long long;
 
 int main()
 {
-    long long n, a;
+    long long n;
     cin >> n;
-    if(n % 2 == 0)
-     a = n / 2;
-    else 
-    a = ((n + 1) / 2) * (-1);
-    cout<< a << endl;
+
+    if (n % 2 == 0)
+    {
+        cout << n / 2;
+    }
+    else
+    {
+        cout << "-" << (n + 1) / 2;
+    }
+    return 0;
 }
