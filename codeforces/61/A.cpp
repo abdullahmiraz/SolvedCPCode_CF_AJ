595e1229a85e8bc90b6cf5f2d6eef308350ff0bd
Abdullah Miraz <abdullahmiraz2015@gmail.com>
2022-11-05 00:24:00 +0600

    Add solution for problem `A - Ultra-Fast Mathematician` Link: https://codeforces.com/contest/61/problem/A Tags: implementation, *800 Ref: https://codeforces.com/contest/61/submission/179302756

---
+++ codeforces/61/A.cpp
@@ -0,0 +1,18 @@
+// Source: https://usaco.guide/general/io
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+	string s1, s2, ans ;
+	cin >> s1 >> s2 ;
+
+	for(int i = 0;i<s1.size();i++){
+		if(s1[i] !=s2[i]){
+			ans+= '1' ;
+		}else{
+			ans+= '0' ;
+		}
+	}
+	cout << ans <<endl ;
+}
