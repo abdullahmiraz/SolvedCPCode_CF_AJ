4d90c8d69b5064f6d9d56eb2e6861e00e95f06d0
Abdullah Miraz <abdullahmiraz2015@gmail.com>
2023-01-28 09:38:00 +0600

    Add solution for problem `B - Taisia and Dice` Link: https://codeforces.com/contest/1790/problem/B Tags: greedy, greedy, math, *800 Ref: https://codeforces.com/contest/1790/submission/190920339

--- codeforces/1790/B.cpp
+++ codeforces/1790/B.cpp
@@ -1,14 +1,35 @@
-#include<bits/stdc++.h>
-using namespace std;
- 
-int main(){
-	int n, s, r;
-	cin >> n;
-	while(cin >> n >> s >> r){
-		cout << s-r; 
-        s -= s-r;
-		for(int i = 1; i < n; i++)
-			cout << ' ' << (s-i+n-1)/(n-1);
-		cout << '\n';
-	}
-}
+#include <bits/stdc++.h>
+    using namespace std;
+    int main() 
+    {
+        int t;
+        cin>>t;
+        while(t--)
+        {
+            int n,s,r,i;
+            cin>>n>>s>>r;
+            int a[n];
+            int t=n;
+            for(i=0;i<n;i++)
+            {
+                if(i==0)
+                {
+                    a[i]=s-r;
+                    s=s-a[i];
+                    t=t-1;
+                }
+                else
+                {
+                    a[i]=s/t;
+                    s=s-a[i];
+                    t=t-1;
+                }
+            }
+            for(i=0;i<n;i++)
+            {
+                cout<<a[i]<<" ";
+            }
+            cout<<endl;
+            
+        }
+    }
